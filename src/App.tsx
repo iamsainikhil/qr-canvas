ff418f7645c6b299fb01ab2a8d0e3512488dd457
iamsainikhil <sainikhil.bheema@gmail.com>
2026-07-18 10:02:43 -0400

    feat: add page-view analytics tracking and new routes (dashboard, scan-error)

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,24 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+import { logEvent } from "firebase/analytics";
 import Index from "./pages/Index";
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
+import ScanError from "./pages/ScanError";
+import { PrivateAppGate } from "./components/PrivateAppGate";
+import { analytics } from "./integrations/firebase/client";
+
+function PageViewTracker() {
+  const location = useLocation();
+  useEffect(() => {
+    if (!analytics) return;
+    logEvent(analytics, 'page_view', { page_path: location.pathname });
+  }, [location.pathname]);
+  return null;
+}
 
 const queryClient = new QueryClient();
 
@@ -14,11 +29,16 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <PrivateAppGate>
+          <PageViewTracker />
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/scan-error" element={<ScanError />} />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </PrivateAppGate>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
