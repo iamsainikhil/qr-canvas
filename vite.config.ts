b541169b512e21d346807bdb14e1d0554d27f9b0
iamsainikhil <sainikhil.bheema@gmail.com>
2026-07-23 11:44:29 -0400

    chore(build): include html files in asset handling

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react()],
+  assetsInclude: ['**/*.html'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
