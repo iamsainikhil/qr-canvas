7dc995f3de46669b0369c7529b8d5533b1d91894
iamsainikhil <sainikhil.bheema@gmail.com>
2026-08-06 15:43:26 -0400

    chore: add basePath and root redirect

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import path from 'path';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/qr-canvas',
   env: {
     NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
